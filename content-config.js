b04ddb69a473532cc5bbdaf05180ee2c66e34c9c
smashedplate73 <smashedplate73@gmail.com>
2025-07-05 13:38:17 +0100

    comm status update

--- content-config.js
+++ content-config.js
@@ -8,7 +8,7 @@ const SiteContent = {
     commissionStatus: {
         status: "CLOSED", // "OPEN", "CLOSED", or "HIATUS"
         slots: {
-            current: 1,
+            current: 2,
             total: 4
         }
     },
